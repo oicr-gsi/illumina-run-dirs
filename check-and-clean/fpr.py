4a65a2910cc92b567276585fb6f763368cf4fafd
Morgan Taschuk <morgan.taschuk@oicr.on.ca>
2022-11-30 17:09:17 -0500

    updated to take annotated skipped and deleted files into account; don't care about more lanes than expected

--- check-and-clean/fpr.py
+++ check-and-clean/fpr.py
@@ -33,6 +33,8 @@ def get_sequencer_run(rname,filetype="chemical/seq-na-fastq-gzip",wfilter="Xenom
                           - sw_size	: the size according to fpr
                           - fs_size	: the size on the filesystem
                           - path	: the path of the file
+                          - skipped : if the file has been skipped
+                          - deleted : whether the file is annotated as deleted
     """
     lanes={}
     with gzip.open(fpr) as tsv:
@@ -49,27 +51,35 @@ def get_sequencer_run(rname,filetype="chemical/seq-na-fastq-gzip",wfilter="Xenom
                 continue
             if filter_matcher.search(line['Workflow Name']):
                 continue
+            
             lane=lanes.setdefault(line['Lane Number'],{})
             barcode=lane.setdefault(line['IUS Tag'],{})
             numfiles=barcode.setdefault('count',0)
-            barcode['count']=(numfiles+1)
             barcode['library']=line['Sample Name']
             details=barcode.setdefault('details',{})
             swid=details.setdefault(line['File SWID'],{})
             filepath=line['File Path']
+            if line['Skip'].strip()=="true":
+                swid['skipped']=True
+            else:
+                swid['skipped']=False
+                barcode['count']=(numfiles+1)
+            deleted=True if "deleted" in line['File Attributes'] else False
             if line['File Size'].strip()=="":
                 swid['sw_size']=0
             else:
                 swid['sw_size']=abs(long(line['File Size']))
-            if os.path.exists(filepath):
+            if not deleted and os.path.exists(filepath):
                 swid['fs_size']=abs(long(os.path.getsize(filepath)))
             else:
                 swid['fs_size']=0
             swid['path']=filepath
+            swid['deleted']=deleted
     return lanes
 
+
 def print_verbose(lane, ius, count, library, swid):
-    return [lane, ius, library,str(count), str(swid['sw_size']), str(swid['fs_size']), swid['path'][:100]+"..."]
+    return [lane, ius, library,str(count),str(swid['skipped']), str(swid['deleted']), str(swid['sw_size']), str(swid['fs_size']), swid['path'][:100]+"..."]
 
 def details(lane,ius,library,problem):
     return "\t".join([lane, ius, library, problem])
@@ -90,13 +100,13 @@ def decisions(fastqs,expected_lanes=8,verbose=False):
     problems=[]
     lanes=fastqs.keys()
     lanes.sort()
-    if len(lanes)!=expected_lanes:
+    if len(lanes)<expected_lanes:
         smallfile=True
         problems.append(["There are "+str(len(lanes))+" lanes and there should be "+str(expected_lanes)])
     if verbose:
         verbose_out=[]
         #verbose_out.append("\t".join(["Lane","Barcode","Library","Count","SW Size", "FS Size", "Path"]))
-    #Iterate through each lane
+    #Iterate through each lane 
     for lane in lanes:
         iuses=fastqs[lane].keys()
         iuses.sort()
@@ -104,18 +114,20 @@ def decisions(fastqs,expected_lanes=8,verbose=False):
         for ius in iuses:
             count=fastqs[lane][ius]['count']
             library=fastqs[lane][ius]['library']
-            #Test if there are more than 2 files per IUS
-            if count > 2:
-                morethantwo=True
-                problems.append(details(lane,ius,library,"Has too many files: "+str(count)))
-            #Test if there are less than 2 files per IUS
-            elif count < 2:
-                lessthantwo=True
-                problems.append(details(lane,ius,library,"Only "+str(count)+" file"))
             swids=fastqs[lane][ius]['details']
+            notdeleted=True
             for swid in swids:
                 sw=swids[swid]['sw_size']
                 fs=swids[swid]['fs_size']
+                deleted=swids[swid]['deleted']
+                skipped=swids[swid]['skipped']
+                if verbose:
+                    verbose_out.append(print_verbose(lane, ius,count,fastqs[lane][ius]['library'],swids[swid]))
+                if deleted:
+                    notdeleted=False
+                    continue    
+                if skipped:
+                    continue
                 #Test if the fpr and filesystem sizes match
                 if sw != fs:
                     mismatchfilesize=True
@@ -125,17 +137,22 @@ def decisions(fastqs,expected_lanes=8,verbose=False):
                     smallfile=True
                     problems.append(details(lane,ius,library,"File very small = "+str(fs)+" bytes"))
                 size=size+fs
-                if verbose:
-                    verbose_out.append(print_verbose(lane, ius,count,fastqs[lane][ius]['library'],swids[swid]))
+            #Test if there are more than 2 files per IUS
+            if count > 2:
+                morethantwo=True
+                problems.append(details(lane,ius,library,"Has too many files: "+str(count)))
+            #Test if there are less than 2 files per IUS
+            elif count < 2 and notdeleted:
+                lessthantwo=True
+                problems.append(details(lane,ius,library,"Only "+str(count)+" file"))
 
-        #Test to see if the lane size is less than 15G
         if verbose:
             print("Lane "+lane+" size:"+str(size/1e9)+"G", file=sys.stderr)
 #        if size/1e9 < 2:
 #            smallfile=True
 #            problems.insert(0,"\t".join(["Lane",lane,"size is <15G:",str(size/1e9)]))
     if verbose and len(verbose_out)>0:
-        pretty_print(["Lane","Barcode","Library","Count","SW Size", "FS Size", "Path"],verbose_out)
+        pretty_print(["Lane","Barcode","Library","Count","Skipped","Deleted","SW Size", "FS Size", "Path"],verbose_out)
     
     if problems:
         import time
